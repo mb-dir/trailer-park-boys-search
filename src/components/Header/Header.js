052fc099f6617cdfbb230a5231e4b8be95b7b6c0
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-10-15 21:34:40 +0200

    Create Header component
    It is simple and static component, so I use stateless component(function)

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+const Header = ()=>{
+    return(
+        <header className="mainHeader">
+            <h1 className="mainHeader__header">Trailer Park Boys Searcher</h1>
+        </header>
+    );
+}
+
+export default Header;
