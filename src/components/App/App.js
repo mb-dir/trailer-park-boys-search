910fa6dd353bcf5d0f2effe3c75be8129085382c
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-10-15 22:19:39 +0200

    Use the Season component
    Now it only shows 7 headers with appropriate titles, in the future it
    will generated the section content

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,18 @@
+import React from "react";
 import Header from "../Header/Header";
+import Season from "../Season/Season";
 function App() {
   return(
-    <Header/>
+    <React.Fragment>
+      <Header/>
+      <Season seasonData={"1st Season"}/>
+      <Season seasonData={"2nd Season"}/>
+      <Season seasonData={"3rd Season"}/>
+      <Season seasonData={"4th Season"}/>
+      <Season seasonData={"5th Season"}/>
+      <Season seasonData={"6th Season"}/>
+      <Season seasonData={"7th Season"}/>
+    </React.Fragment>
   );
 }
 
