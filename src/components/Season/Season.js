ed23c34e1d7c68faae852e43a2613529cc56af90
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-10-16 22:50:14 +0200

    Add mock state structure

--- src/components/Season/Season.js
+++ src/components/Season/Season.js
@@ -1,5 +1,22 @@
 import React from "react";
 class Season extends React.Component{
+    // Example state structure - single season will be passed to another component which will create the appropriate content
+    state = {
+        seasons: [
+            {
+                id:0,
+                titles:["title 1", "title 2"],
+            },
+            {
+                id:1,
+                titles:["title 11", "title 22"],
+            },
+            {
+                id:2,
+                titles:["title 111", "title 222"],
+            },
+        ],
+    }
     render(){
         return(
             <h2>{this.props.seasonData}</h2>
