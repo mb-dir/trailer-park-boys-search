2369019a41998a0320b89a895f0ab03d0eaaf005
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-10-17 21:22:53 +0200

    Delete unnecessary wrapper
    This div was totaly unneecessary, cuz section can be treates as a
    wrapper

--- src/components/SeasonContent/SeasonContent.js
+++ src/components/SeasonContent/SeasonContent.js
@@ -3,11 +3,9 @@ import React from "react";
 const SeasonContent = (props)=>{
     const seasonContent = props.data.map((el)=>{
         return(
-            <React.Fragment>
-                <section>
-                    <h2>{el.titles}</h2>
-                </section>
-            </React.Fragment>
+            <section>
+                <h2>{el.titles}</h2>
+            </section>
         );
     });
     return(
