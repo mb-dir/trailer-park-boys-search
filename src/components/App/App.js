431432d547ebb7fc7becdc9c154ede6de41771ff
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-10-17 11:12:26 +0200

    Delete unnecessary Season components calling
    I have changed the approach to this problem, now there is only one
    Season component, and the SeasonContent component will create the whole
    content for wach season

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,13 +5,7 @@ function App() {
   return(
     <React.Fragment>
       <Header/>
-      <Season seasonData={{title: "1st Season", id:0}}/>
-      <Season seasonData={{title: "2nd Season", id:1}}/>
-      <Season seasonData={{title: "3rd Season", id:2}}/>
-      <Season seasonData={{title: "4th Season", id:3}}/>
-      <Season seasonData={{title: "5th Season", id:4}}/>
-      <Season seasonData={{title: "6th Season", id:5}}/>
-      <Season seasonData={{title: "7th Season", id:6}}/>
+      <Season/>
     </React.Fragment>
   );
 }
