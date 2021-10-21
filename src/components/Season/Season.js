f683e76cc9951e04cc81925b519458a327767e39
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-10-21 20:12:48 +0200

    Other state structure
    In order to have posibility to hide only one title I have to keep it on
    objects array, and each objec has hidden property which determinates if
    the title has to be hidden

--- src/components/Season/Season.js
+++ src/components/Season/Season.js
@@ -8,31 +8,8 @@ class Season extends React.Component{
         seasons: [
             {
                 id:0,
-                titles:["title 1 season 1", "title 2 season 1", "title 3 season 1", "title 4 season 1", "title 5 season 1"],
-            },
-            {
-                id:1,
-                titles:["title 1 season 2", "title 2 season 2", "title 3 season 2", "title 4 season 2", "title 5 season 2"],
-            },
-            {
-                id:2,
-                titles:["title 1 season 3", "title 2 season 3", "title 3 season 3", "title 4 season 3", "title 5 season 3"],
-            },
-            {
-                id:3,
-                titles:["title 1 season 4", "title 2 season 1", "title 3 season 1", "title 4 season 1", "title 5 season 4"],
-            },
-            {
-                id:4,
-                titles:["title 1 season 5", "title 2 season 1", "title 3 season 1", "title 4 season 1", "title 5 season 5"],
-            },
-            {
-                id:5,
-                titles:["title 1 season 6", "title 2 season 1", "title 3 season 1", "title 4 season 1", "title 5 season 6"],
-            },
-            {
-                id:6,
-                titles:["title 1 season 7", "title 2 season 1", "title 3 season 1", "title 4 season 1", "title 5 season 7",],
+                // episodeID 11 means 1st season and 1st episode, 12 means 1st season and 2nd episode etc 
+                titles:[{title: "title 1 season 1", episodeID:11, hidden: false}, {title: "title 2 season 1", episodeID:12, hidden: false}, {title: "title 3 season 1", episodeID:13, hidden: false}, {title: "title 4 season 1", episodeID:14, hidden: false}, {title: "title 5 season 1", episodeID:15, hidden: false}],
             },
 
         ],
@@ -59,42 +36,6 @@ class Season extends React.Component{
                     <h2 className="seasons__titleSeason">Season 1</h2>
                     <div className="seasons__content">
                         <SeasonContent data={this.state.seasons[0]}/>
-                    </div>
-                </section>
-                <section className="seasons__season">
-                    <h2 className="seasons__titleSeason">Season 2</h2>
-                    <div className="seasons__content">
-                        <SeasonContent data={this.state.seasons[1]}/>
-                    </div>
-                </section>
-                <section className="seasons__season">
-                    <h2 className="seasons__titleSeason">Season 3</h2>
-                    <div className="seasons__content">
-                        <SeasonContent data={this.state.seasons[2]}/>
-                    </div>
-                </section>
-                <section className="seasons__season">
-                    <h2 className="seasons__titleSeason">Season 4</h2>
-                    <div className="seasons__content">
-                        <SeasonContent data={this.state.seasons[3]}/>
-                    </div>
-                </section>
-                <section className="seasons__season">
-                    <h2 className="seasons__titleSeason">Season 5</h2>
-                    <div className="seasons__content">
-                        <SeasonContent data={this.state.seasons[4]}/>
-                    </div>
-                </section>
-                <section className="seasons__season">
-                    <h2 className="seasons__titleSeason">Season 6</h2>
-                    <div className="seasons__content">
-                        <SeasonContent data={this.state.seasons[5]}/>
-                    </div>
-                </section>
-                <section className="seasons__season">
-                    <h2 className="seasons__titleSeason">Season 7</h2>
-                    <div className="seasons__content">
-                        <SeasonContent data={this.state.seasons[6]}/>
                     </div>
                 </section>
             </main>
