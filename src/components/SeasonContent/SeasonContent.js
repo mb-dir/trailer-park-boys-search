6799ee59d2a7a1aed27ea33ed035c92fb5a2075f
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-10-25 20:04:50 +0200

    Delete unnecessary console.log

--- src/components/SeasonContent/SeasonContent.js
+++ src/components/SeasonContent/SeasonContent.js
@@ -5,7 +5,6 @@ const SeasonContent = (props)=>{
     const seasonContent = props.data.titles.map((el)=>{
         let className;
         el.hidden ? className = "season__episode season__episode--hidden episode" : className = "season__episode episode";
-        console.log(className)
         return(
             <div className={className} key={el.episodeID}>
                 <h3 className="episode__title" >{el.title}</h3>
