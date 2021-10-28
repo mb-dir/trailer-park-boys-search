54233888138fe0e98ab8d65f2d0854844630c783
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-10-28 23:20:03 +0200

    Prepare the link

--- src/components/SeasonContent/SeasonContent.js
+++ src/components/SeasonContent/SeasonContent.js
@@ -7,7 +7,7 @@ const SeasonContent = (props)=>{
         el.hidden ? className = "season__episode season__episode--hidden episode" : className = "season__episode episode";
         return(
             <div className={className} key={el.episodeID}>
-                <a className="episode__title" href="google.com">{el.title}</a>
+                <a className="episode__title" target="_blank" href={el.linkToEpisode}>{el.title}</a>
             </div>
         );
     });
