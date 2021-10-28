f7cd9d6e7fc8de54e60c0bf5da6f6c3a41501538
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-10-28 22:57:20 +0200

    Change the SeasonContent return content
    Instead of returning <h3></h3> return the <a></a> cuz it will be link to
    the episode - now it refers to google, but I will add something like
    "linkToEpisode" in state in Season and based on this link I will create
    href in <a></a>

--- src/components/SeasonContent/SeasonContent.js
+++ src/components/SeasonContent/SeasonContent.js
@@ -7,7 +7,7 @@ const SeasonContent = (props)=>{
         el.hidden ? className = "season__episode season__episode--hidden episode" : className = "season__episode episode";
         return(
             <div className={className} key={el.episodeID}>
-                <h3 className="episode__title" >{el.title}</h3>
+                <a className="episode__title" href="google.com">{el.title}</a>
             </div>
         );
     });
