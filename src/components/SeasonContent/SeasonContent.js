7b2a8fae3e44316b639e855fd104c6395d08256f
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-10-30 14:43:06 +0200

    Change structure of returned element
    Add classes in order to style

--- src/components/SeasonContent/SeasonContent.js
+++ src/components/SeasonContent/SeasonContent.js
@@ -6,8 +6,11 @@ const SeasonContent = (props)=>{
         let className;
         el.hidden ? className = "season__episode season__episode--hidden episode" : className = "season__episode episode";
         return(
-            <a className="episode__title" target="_blank" href={el.linkToEpisode}>
-                <div className={className} key={el.episodeID}>{el.title}</div>
+            <a className="episode__titleContainer" target="_blank" href={el.linkToEpisode}>
+                <div className={className} key={el.episodeID}>
+                    <h3 className="episode__title">{el.title}</h3>
+                    <img className="episode__thumbnail" src={`${el.thumbnail}`} alt="episode thumbnail"/>
+                </div>
             </a>
         );
     });
