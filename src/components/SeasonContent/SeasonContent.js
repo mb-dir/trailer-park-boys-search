e79d158fb731442ca50df39060ad1f5575592013
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-10-30 14:52:12 +0200

    Add rel attribute to <a> with target _blank
    Using target="_blank" without rel="noreferrer" (which implies
    rel="noopener") is a security risk in older browsers: see
    https://mathiasbynens.github.io/rel-noopener/#recommendations
    react/jsx-no-target-blank

--- src/components/SeasonContent/SeasonContent.js
+++ src/components/SeasonContent/SeasonContent.js
@@ -6,7 +6,7 @@ const SeasonContent = (props)=>{
         let className;
         el.hidden ? className = "season__episode season__episode--hidden episode" : className = "season__episode episode";
         return(
-            <a className="episode__titleContainer" target="_blank" href={el.linkToEpisode}>
+            <a className="episode__titleContainer" target="_blank" rel="noreferrer" href={el.linkToEpisode}>
                 <div className={className} key={el.episodeID}>
                     <h3 className="episode__title">{el.title}</h3>
                     <img className="episode__thumbnail" src={`${el.thumbnail}`} alt="episode thumbnail"/>
